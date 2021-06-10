d91e88b3ab03e3c0af70af07eab50af7fe3dbf80
Imran Saleem <imran.sheikh834@gmail.com>
2021-06-10 20:30:55 +0500

    Initial Commit

---
+++ database/migrations/2021_05_22_153016_create_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('location_name');
+            $table->string('street');
+            $table->string('city');
+            $table->integer('lat');
+            $table->integer('lng');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
---
+++ database/migrations/2021_05_22_155431_create_deal_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDealProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deal_products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('product_id');
+            $table->string('name');
+            $table->integer('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deal_products');
+    }
+}
